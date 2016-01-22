1ea9caefc1c06cea9d41df59afda40c25d3cdc86
kenjis <kenji.uui@gmail.com>
2016-01-22 09:52:15 +0900

    Update init.sh and update.sh

--- init.sh
+++ init.sh
@@ -2,15 +2,17 @@
 
 git clone git@github.com:bcit-ci/CodeIgniter.git
 
-# Create git repo containing only user_guide_src/
+# Create git repo only containing user_guide_src/
 rm -rf user_guide_src_en
 git clone CodeIgniter user_guide_src_en
-cd user_guide_src_en
+cd user_guide_src_en/
 git filter-branch --prune-empty --subdirectory-filter user_guide_src develop
 
-cd ..
-git clone user_guide_src_en user_guide_src_ja
+cd ../
+git clone git@github.com:codeigniter-jp/user_guide_src_ja.git
 
-cd user_guide_src_ja
+cd user_guide_src_ja/
+git checkout -b develop --track origin/develop
+git remote add en ../user_guide_src_en/
 git remote add nekoget git@github.com:NEKOGET/ci_user_guide_src.git
 git fetch nekoget
--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-cd CodeIgniter
+cd CodeIgniter/
 git fetch origin
 git checkout develop
 git merge origin/develop
-cd ..
+cd ../
 
 # Create git repo only containing user_guide_src/
 rm -rf user_guide_src_en
 git clone CodeIgniter user_guide_src_en
-cd user_guide_src_en
+cd user_guide_src_en/
 git filter-branch --prune-empty --subdirectory-filter user_guide_src develop
 
-cd ../user_guide_src_ja
-git fetch origin
+cd ../user_guide_src_ja/
+git fetch en
 git checkout develop
-git merge origin/develop
+git merge en/develop
