8554fa4fced238cf59ecca244a28b947f0dec14b
kenjis <kenji.uui@gmail.com>
2016-01-22 21:38:17 +0900

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -7,5 +7,8 @@ make html
 cd ../
 php cmd.php add:link
 
+cp -p user_guide_src_ja/CREDITS.md user_guide_src_ja/build/html/
+cp -p user_guide_src_ja/LICENSE.md user_guide_src_ja/build/html/
+
 echo
 echo "Open user_guide_src_ja/build/html/index.html"
