ba83f2033b497d735fcf6071c6074c757b296437
kenjis <kenji.uui@gmail.com>
2016-01-22 22:09:21 +0900

    Fix indent

--- meld-ci-ja.sh
+++ meld-ci-ja.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-        echo "meld ci-ja/user_guide_ja_src/source user_guide_src_ja/source"
-        echo " usage: $0 file"
-        echo "    eg: $0 tutorial/static_pages"
-        exit;
+    echo "meld ci-ja/user_guide_ja_src/source user_guide_src_ja/source"
+    echo " usage: $0 file"
+    echo "    eg: $0 tutorial/static_pages"
+    exit;
 fi
 
 file="$1"
--- meld-en.sh
+++ meld-en.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-        echo "meld user_guide_src_en/source user_guide_src_ja/source"
-        echo " usage: $0 file"
-        echo "    eg: $0 tutorial/static_pages"
-        exit;
+    echo "meld user_guide_src_en/source user_guide_src_ja/source"
+    echo " usage: $0 file"
+    echo "    eg: $0 tutorial/static_pages"
+    exit;
 fi
 
 file="$1"
